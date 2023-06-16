fc737c27f4abffd7f08e2e9a6db820423d16eefc
kaaxem <mmdkazemm@gmail.com>
2023-06-16 21:06:52 +0330

    new components

--- ventilimos/src/components/Home/Home.jsx
+++ ventilimos/src/components/Home/Home.jsx
@@ -4,7 +4,11 @@ export default function Home() {
   return (
     <div className="home" id="home">
       <div className="imgContainer">
-       <img className="img" src="/assests/trees.jpg"/>
+       <img className="img" src="/assests/trees.jpg" alt=""/>
+      </div>
+      <div className="text">
+       <p>Navigate the intricate landscape of technology management and pave the 
+        way for your success</p>
       </div>
     </div>
   )
