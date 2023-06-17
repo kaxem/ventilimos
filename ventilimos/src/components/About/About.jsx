40a3246e8c8d1087087cfb2a9c5250bdbeec35b4
kaaxem <mmdkazemm@gmail.com>
2023-06-17 22:15:33 +0330

    portfolio added

--- ventilimos/src/components/About/About.jsx
+++ ventilimos/src/components/About/About.jsx
@@ -3,24 +3,25 @@ import "./About.scss";
 export default function About() {
   return (
     <div className="about" id="about">
-      <div className="description">
-      <h2>Ventilimos is dedicated to:</h2>
-      <br/>
-      <p>
-        Vision<br/>
-        To support tech-driven SMBS and professinals, fostering growth and <br/>
-        professionals satisfaction in competetive markets.
-      </p>
-      <br/>
-      <p>
-        Mission<br/>
-        To be your growth partner, wheter you're a scaling business or an individual <br/>
-        transitioning into leadership.
-      </p>
-      </div>
       <div className="imgContainer">
         <img className="img" src="/assests/lighthouse.jpg" alt=""/>
       </div>
+      <div className="description">
+        <h2>Ventilimos is dedicated to:</h2>
+        <br/>
+        <p>
+         <h4>Vision</h4>
+         To support tech-driven SMBS and professinals, fostering growth and <br/>
+         professionals satisfaction in competetive markets.
+        </p>
+        <br/>
+        <p>
+          <h4>Mission</h4>
+          To be your growth partner, wheter you're a scaling business or an individual <br/>
+          transitioning into leadership.
+       </p>
+      </div>
+    
     </div>
   )
 }
--- ventilimos/src/components/Portfolio/Portfolio.jsx
+++ ventilimos/src/components/Portfolio/Portfolio.jsx
@@ -4,6 +4,53 @@ export default function Portfolio() {
   return (
     <div className="Portfolio" id="portfolio">
       
+      <div className="content">
+      <div className="imgContainer">
+          <img className="img" src="/assests/cv2.jpg" alt=""/>
+        </div>
+        <div className="description">
+        <h2>About me</h2>
+          <p>
+            I am Christopher Lederer, a dedicated professional who turned his hobby into a
+            fulfilling career. In 1986, I got my first personal computer, and by 1990, I was building
+            my own from scrap parts. This early interest developed into a strong background in
+            system administration and hardware.
+          </p>
+          <br/>
+          <p>
+            From 1992 to 1999, I provided telecommunications and fax solutions for various
+            businesses, coupling this practical experience with formal education as I pursued a
+            egree in Medicine from 1992 to 1998. Studying medicine gave me a deep
+            understanding of accountability, clear communication, and expectation management,
+            values that I still uphold in my professional approach.
+          </p>
+          <br/>
+          <p>
+            In 1999, I embarked on a career in technology management and leadership. I've held
+            various executive roles, in notable companies specialising in executive management,
+            organisational structuring, process management, strategic and operational technology
+            management, and team development.
+          </p>
+          <br/>
+          <p>
+            Throughout my journey, I've come to understand that success in the tech industry
+            goes beyond just technical knowledge. It involves clear communication, efficient
+            systems, realistic expectations, and a deep understanding of the unique culture of
+            each organisation. My experience working across different countries and continents
+            has cultivated a culturally adept approach that allows me to adapt to diverse work
+            environments effectively.
+          </p>
+          <br/>
+
+          <p>
+            These insights and experiences led me to establish Ventilimos Consulting. Here, I share
+            my strategies with businesses and individuals, guiding them through the complexities
+            of scaling and leadership transition.
+          </p>
+        </div>
+        
+      </div>
+      
     </div>
   )
 }
