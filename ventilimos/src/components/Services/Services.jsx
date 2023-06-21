db6f8ebdec0b89b310b7c957fcb4a8232cb36c6e
kaaxem <mmdkazemm@gmail.com>
2023-06-22 00:26:36 +0330

    new commit

--- ventilimos/src/components/Services/Services.jsx
+++ ventilimos/src/components/Services/Services.jsx
@@ -3,7 +3,62 @@ import "./Services.scss"
 export default function Services() {
   return (
     <div className="Services" id="services">
-      
-    </div>
+        <div className="imgContainer">
+          <img src="/assests/horizont.jpg" alt=""/>
+          </div>
+          <div className="bottom">
+            <div className="left">
+              <div className="p1">
+                  <h3>Growth Management:</h3>
+                  <br/>
+                  <p>Guiding through scaling complexities,
+                      creating scalable processes, and fostering
+                      leadership growth.</p>
+              </div>
+              <div className="p2">
+                  <h3>Strategy:</h3><br/>
+                  <p>
+                  Partnering to align your business's vision,
+                  mission, strategy, and tactics for a unified
+                  direction.
+                  </p>
+              </div>
+              <div className="p3">
+                <h3>Trust Building:</h3><br/>
+                <p>
+                Providing strategies to cultivate trust
+                within you and your teams.
+                </p>
+              </div>
+            </div>
+            <div className="right">
+                <div className="p1">
+                <h3>Communication:</h3><br/>
+                  <p>
+                  Promoting a culture of information sharing
+                  and aligning expectations throughout the
+                  organisation.
+                  </p>
+                </div>
+              <div className="p2">
+              <h3>Personal Growth Goals:</h3><br/>
+                <p>
+                Working with you to identify strengths,
+                areas of improvement, and setting
+                achievable growth goals.
+                </p>
+                </div>
+              <div className="p3">
+                <h3>Management Skills Development:</h3><br/>
+                  <p>
+                  Sharing strategies for successful
+                  managerial transition, enhancing
+                  communication, and leadership skills
+                  </p>
+              </div>
+            </div>
+          </div>
+      </div>
+
   )
 }
