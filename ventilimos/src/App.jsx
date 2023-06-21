439c6b5a7b193ffa70c3fd2e1a256c68fde83d5d
kaxem <mmdkazemm@gmail.com>
2023-06-21 12:44:12 +0330

    Mostafa

--- ventilimos/src/App.jsx
+++ ventilimos/src/App.jsx
@@ -5,12 +5,31 @@ import Portfolio from "./components/Portfolio/Portfolio";
 import Services from "./components/Services/Services";
 import Contact from "./components/Contact/Contact";
 import "./App.scss";
+import { useState } from "react";
+
 
 function App() {
+  const [style,setStyle]=useState(['black','gray','gray','gray','gray'])
+
+  function scorllNav (event){
+  console.log(event.target.scrollTop)
+
+  if (event.target.scrollTop<670){
+    setStyle(['black','gray','gray','gray','gray'])
+  }else if(670<=event.target.scrollTop && event.target.scrollTop<1340 ){
+    setStyle(['gray','black','gray','gray','gray'])
+  }else if(1340<=event.target.scrollTop && event.target.scrollTop<2010){
+    setStyle(['gray','gray','black','gray','gray'])
+  }else if(2010<=event.target.scrollTop && event.target.scrollTop<2680){
+    setStyle(['gray','gray','gray','black','gray'])
+  }else if(2680<=event.target.scrollTop){
+    setStyle(['gray','gray','gray','gray','black'])
+  }
+};
   return (
-    <div className="app">
-      <Topbar/>
-      <div className="sections">
+    <div className="app" >
+      <Topbar style={style}/>
+      <div className="sections" onScroll={scorllNav}>
         <Home/>
         <About/>
         <Portfolio/>
--- ventilimos/src/components/Topbar/Topbar.jsx
+++ ventilimos/src/components/Topbar/Topbar.jsx
@@ -1,6 +1,6 @@
 import "./Topbar.scss"
 
-export default function Topbar() {
+export default function Topbar({style}) {
 function test(e){
   e.target.classList.add("change");
 }
@@ -8,11 +8,11 @@ function test(e){
     <div className="topbar">
         <div className="wrapper">
             <div className="left">
-                <a href="#home" className="menu" onClick={test}>Home</a>
-                <a href="#about" className="menu">About</a>
-                <a href="#portfolio" className="menu">Me</a>
-                <a href="#services" className="menu">Services</a>
-                <a href="#contact" className="menu">Contact</a>
+                <a href="#home" className="menu" style={{color:style[0]}} onClick={test}>Home</a>
+                <a href="#about" className="menu" style={{color:style[1]}} >About</a>
+                <a href="#portfolio" className="menu" style={{color:style[2]}} >Me</a>
+                <a href="#services" className="menu" style={{color:style[3]}} >Services</a>
+                <a href="#contact" className="menu" style={{color:style[4]}} >Contact</a>
             </div>
             <div className="title">
                 <a href="/"className="logo">Ventilimos Consulting</a>
